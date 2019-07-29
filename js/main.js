56a91852c417490f59b5354f4d3bfe82b75874d9
lkm543 <lkm543@gmail.com>
2019-07-29 14:04:31 +0800

    Restructure

--- js/main.js
+++ js/main.js
@@ -18,13 +18,13 @@ $(document).ready( function() {
 	});	
 function animMeter(){
     $(".meter > span").each(function() {
-                $(this)
-                    .data("origWidth", $(this).width())
-                    .width(0)
-                    .animate({
-                        width: $(this).data("origWidth")
-                    }, 1200);
-            });
+        $(this)
+            .data("origWidth", $(this).width())
+            .width(0)
+            .animate({
+                width: $(this).data("origWidth")
+            }, 1200);
+    });
 }
 animMeter();
 
